b22b2433d2c3126a5819b7aa29e8e52974cd9a08
neville777 <neville.james@student.moringaschool.com>
2023-09-28 12:55:06 +0300

    mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+document.addEventListener("DOMContentLoaded", function () {
+  const taskForm = document.getElementById("create-task-form");
+
+  taskForm.addEventListener("submit", function (e) {
+    // Prevent page from refreshing
+
+    e.preventDefault();
+
+     // Remove whitespace from the input field
+
+    const taskDescription = document.getElementById("new-task-description").value;
+      // Check if the description is not empty
+
+    if (taskDescription !== "") { 
+      addTaskToDOM(taskDescription);
+
+      // Clear the input field
+
+      document.getElementById("new-task-description").value = "";
+    } else {
+      // Show an alert if the description is empty
+      alert("Weka Jina"); 
+    }
+  });
+
+  function addTaskToDOM(description) {
+    const taskItem = document.createElement("li");
+    taskItem.textContent = description;
+
+    // Create a remove button
+
+    const removeButton = document.createElement("button"); 
+    removeButton.textContent = "NGOA";
+    removeButton.addEventListener("click", (e) => {
+      // Remove the task item when the button is clicked
+      e.target.parentNode.remove(); 
+    });
+
+    // Add the remove button to the task item
+
+    taskItem.appendChild(removeButton); 
+
+    const taskList = document.getElementById("tasks");
+    taskList.appendChild(taskItem);
+  }
 });
